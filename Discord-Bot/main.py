b63a80ffde8c66608ce40298a33322c579c41e73
TBetter00 <125780472+TBetter00@users.noreply.github.com>
2025-09-22 23:59:53 +0700

    add discord bot test
    Co-Authored-By: Diawurbro <220590301+Diawurbro@users.noreply.github.com>

---
+++ Discord-Bot/main.py
@@ -0,0 +1,20 @@
+import discord
+from discord.ext import commands
+import logging
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log',encoding='utf-8',mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
+
+client = discord.Client(intents=intents)
+
+#Channel
+test_channel = 1419714011812991096
+
+client.run(token)
