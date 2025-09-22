ceb01aac12caf4caf68d60c8249d0f19126012eb
azur <natapat.samutpong@gmail.com>
2025-09-22 19:28:52 +0700

    Test WebSocket data sending
    Co-Authored-By: teerapon2549 <165137232+teerapon2549@users.noreply.github.com>

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,32 @@
-import { Elysia } from "elysia"
+import { Elysia, t } from "elysia"
+import { cors } from "@elysiajs/cors"
+
+const state = {
+  count: 0
+}
 
 const app = new Elysia()
-  .get("/", () => "Hello Elysia")
+  .use(cors())
+  .ws("/count", {
+    open(ws) {
+      ws.subscribe("count")
+    },
+    message(ws, msg) {
+      if (typeof msg === "string" && msg === "get") {
+        ws.publish("count", state.count)
+      }
+    }
+  })
+  .post("/api/count/incr", ({ body }) => {
+    state.count += body.by
+    app.server?.publish("count", `${state.count}`)
+    return state.count
+  }, {
+    body: t.Object({
+      by: t.Number()
+    })
+  })
   .listen(3000)
 
 console.log("Server running at http://localhost:3000")
+export type App = typeof app
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,50 @@
-import { useState } from 'react'
+import { useEffect, useState } from "react"
 
-function App() {
+const incrementCount = async (by: number) => {
+  await fetch("http://localhost:3000/api/count/incr", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ by })
+  })
+}
+
+function Main() {
   const [count, setCount] = useState(0)
+  const [incrementBy, setIncrementBy] = useState(1)
+
+  useEffect(() => {
+    const ws = new WebSocket("ws://localhost:3000/count")
+
+    ws.onopen = () => {
+      ws.send("get")
+    }
+
+    ws.onmessage = (event) => {
+      console.log("Message from server:", event.data)
+      setCount(Number(event.data))
+    }
+  }, [])
 
   return (
     <>
       <div className="h-screen flex flex-col items-center justify-center">
+        <input
+          type="number"
+          value={incrementBy}
+          onChange={(e) => setIncrementBy(Number(e.target.value))}
+          className="mb-4 px-2 py-1 border border-gray-300 rounded"
+        />
         <button
           className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600"
-          onClick={() => setCount((count) => count + 1)}
+          onClick={() => incrementCount(incrementBy)}
         >
-          count is {count}
+          Count is {count ?? "Loading..."}
         </button>
       </div>
     </>
   )
 }
 
-export default App
+export default Main
