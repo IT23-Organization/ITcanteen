7b181b41ceb1df618e057aabb61079b4f791d149
Diawurbro <diawwy4@gmail.com>
2025-10-04 18:55:30 +0700

    Ticket
    Add bot respond to ticket channels
    (not tested yet)

--- Discord-Bot/main.py
+++ Discord-Bot/main.py
@@ -7,14 +7,32 @@ import os
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
 
-handler = logging.FileHandler(filename='discord.log',encoding='utf-8',mode='w')
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
 
-client = discord.Client(intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-#Channel
+@bot.event
+async def on_ready():
+    print(f"{bot.user.name} ready to go!")
+
+#menu command
+@bot.command()
+async def menu(ctx):
+    await ctx.send(file=discord.File('Discord-Bot/image/menu.jpg'))
+
+# Respond to messages in ticket channels
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+    if message.channel.name.startswith("ticket-"):
+        if "open ticket" in message.content.lower():
+            await message.channel.send(file=discord.File('image/menu.jpg'))
+    await bot.process_commands(message)
+
 test_channel = 1419714011812991096
 
-client.run(token)
+bot.run(token)
