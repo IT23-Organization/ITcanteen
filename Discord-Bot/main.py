229bdc06c3cfe6950458f5d0532a1b9445567697
Diawurbro <diawwy4@gmail.com>
2025-10-04 19:24:45 +0700

    Add the restaurant select
    (not tested yet)
    
    Co-Authored-By: PNPRLL <170003342+PNPRLL@users.noreply.github.com>
    Co-Authored-By: TBetter00 <125780472+TBetter00@users.noreply.github.com>

--- Discord-Bot/main.py
+++ Discord-Bot/main.py
@@ -30,7 +30,19 @@ async def on_message(message):
         return
     if message.channel.name.startswith("ticket-"):
         if "open ticket" in message.content.lower():
+            await message.channel.send("These are the restaurant in the canteen:\n1. Mama\n2. Pizza\n3. KFC\n4. Starbucks\n5. McD\nType !menu to see the menu.")
+        if message.content.lower() == "!menu mama":
             await message.channel.send(file=discord.File('image/menu.jpg'))
+        elif message.content.lower() == "!menu pizza":
+            await message.channel.send(file=discord.File('image/menu.jpg'))
+        elif message.content.lower() == "!menu kfc":
+            await message.channel.send(file=discord.File('image/menu.jpg'))
+        elif message.content.lower() == "!menu starbucks":
+            await message.channel.send(file=discord.File('image/menu.jpg'))
+        elif message.content.lower() == "!menu mcd":
+            await message.channel.send(file=discord.File('image/menu.jpg'))
+        else:
+            await message.channel.send("Sorry, I don't have the menu for that restaurant.")
     await bot.process_commands(message)
 
 test_channel = 1419714011812991096
